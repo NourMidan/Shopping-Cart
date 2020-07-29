b435ea9bf511b51036da43b10bc31ae300aed9bb
NourMidan <58523548+NourMidan@users.noreply.github.com>
2020-07-29 14:33:38 +0200

    Updates

---
+++ precache-manifest.25eab3a915fa111e9f98a900661c565a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6f066c8a93be6821d03dc91a197430",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "81e499a6bcefec64f360",
+    "url": "/Shopping-Cart/static/css/main.d8bf3c71.chunk.css"
+  },
+  {
+    "revision": "4cbadaeebd52d1e7530c",
+    "url": "/Shopping-Cart/static/js/2.9e618f8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shopping-Cart/static/js/2.9e618f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e499a6bcefec64f360",
+    "url": "/Shopping-Cart/static/js/main.cc3e556f.chunk.js"
+  },
+  {
+    "revision": "dc4ddcf3b8b5b79ccf07",
+    "url": "/Shopping-Cart/static/js/runtime-main.ed685d3e.js"
+  },
+  {
+    "revision": "ef46fe4d6662b06ea32329d750c58a75",
+    "url": "/Shopping-Cart/static/media/Google.ef46fe4d.png"
+  },
+  {
+    "revision": "0584c3d2b0e632e241d6446eb042eef6",
+    "url": "/Shopping-Cart/static/media/HP.0584c3d2.png"
+  },
+  {
+    "revision": "423cb43245bb2dfae15ab806460563ed",
+    "url": "/Shopping-Cart/static/media/acer.423cb432.png"
+  },
+  {
+    "revision": "7a62c180fe27573a05f89bce3a4fb9be",
+    "url": "/Shopping-Cart/static/media/asus.7a62c180.png"
+  },
+  {
+    "revision": "cfcad939e070d8394327f9bf3eba4bea",
+    "url": "/Shopping-Cart/static/media/iphone-8.cfcad939.png"
+  },
+  {
+    "revision": "331f4d5df2635cf2651a253f8d9193d1",
+    "url": "/Shopping-Cart/static/media/iphone-se.331f4d5d.png"
+  },
+  {
+    "revision": "e3c812e1c3ec6e93ccbeb1c42baf2e05",
+    "url": "/Shopping-Cart/static/media/lg.e3c812e1.png"
+  },
+  {
+    "revision": "21e2b7e97c5717a897343e28294232c0",
+    "url": "/Shopping-Cart/static/media/one-plus.21e2b7e9.png"
+  },
+  {
+    "revision": "003af1a90a445fedce0cf4a7e8fde7c9",
+    "url": "/Shopping-Cart/static/media/samsung.003af1a9.png"
+  },
+  {
+    "revision": "b5eca2842155b4a67c49636fe6e7c959",
+    "url": "/Shopping-Cart/static/media/samsungtv.b5eca284.png"
+  },
+  {
+    "revision": "3fa2ec328cfe36ad187666603ee48748",
+    "url": "/Shopping-Cart/static/media/sonytv.3fa2ec32.png"
+  }
+]);
